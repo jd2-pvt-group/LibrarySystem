ad8abba0f569df8b456fd9eddcaacff3b96e9403
by-oleg-sokol <sokol.oleg.java@yandex.ru>
2014-01-20 13:37:11 +0300

    Initial state of project

---
+++ LibrarySystem/backend/src/main/java/com/pvt/jd2/project/back/App.java
@@ -0,0 +1,13 @@
+package com.pvt.jd2.project.back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
