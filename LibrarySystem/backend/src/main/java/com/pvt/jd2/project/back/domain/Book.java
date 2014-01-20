272694f5ed270972275b4ee1aedc2a4091e32a90
by-oleg-sokol <sokol.oleg.java@yandex.ru>
2014-01-20 16:53:11 +0300

    domain objects

---
+++ LibrarySystem/backend/src/main/java/com/pvt/jd2/project/back/domain/Book.java
@@ -0,0 +1,12 @@
+package com.pvt.jd2.project.back.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Oleg
+ * Date: 20.01.14
+ * Time: 16:48
+ */
+public class Book implements Serializable {
+}
